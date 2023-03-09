772246f3673224cc5eb3aba5a659339015d5d203
negin <nsouri7979@gmail.com>
2023-03-09 21:24:02 +0430

    delet this component

--- reactShop/src/components/pagination/index.jsx
+++ reactShop/src/components/pagination/index.jsx
@@ -1,15 +1,16 @@
-// import React, { useState } from 'react'
-// import { Pagination } from '@mui/material'
-// const Pagination = () => {
-//     const [page,setPage]=useState(1);
-//     const handlePagination=(event,value)=>{
-//         setPage(value)
-//     }
-//   return (
-//     <div>
-//       <Pagination count={3} onChange={handlePagination}/>
-//     </div>
-//   )
-// }
+import React, { useState } from 'react'
+import { Pagination } from '@mui/material'
+const Pagination = () => {
+    const [page,setPage]=useState(0);
+    const[rowsPerPage,setRowsPerPage]=useState(5)
+    const handlePagination=(event,value)=>{
+        setPage(value)
+    }
+  return (
+    <div>
+<Pagination count={10} color="secondary" onChange={handlePagination}/>
+    </div>
+  )
+}
 
-// export default Pagination
+export default Pagination
