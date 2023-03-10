d3840141bdae7568d832c54b05b4ccdb77e1b837
negin <nsouri7979@gmail.com>
2023-03-10 12:28:14 +0430

    change my router

--- reactShop/src/providers/AppRouterProvider.jsx
+++ reactShop/src/providers/AppRouterProvider.jsx
@@ -1,25 +1,34 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Basket from "../components/basket";
-// import FinalBasket from "../components/finalBasket";
 import Paymentgateway from "../components/paymentgateway";
-// import ProductManaging from "../components/productManaging";
 import AdminPanel from "../pages/adminPanel";
 import Auth from '../pages/auth/index'
-import Home from "../pages/home";
+import Home from "../pages/home/index";
 import NotFound from '../pages/notFound'
-
+import Basket from "../components/basket";
+import FinalAuth from "../components/finalAuth";
+import CartDetail from '../components/cartDetail/index'
+import Layout from "../layouts/layout";
+import Carts from "../components/carts";
 
 const AppRouterProvider = () => {
+
   return (
-    <BrowserRouter>
+
+<BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />}/>
+        <Route path='/' element={< Layout/>}  >
+       <Route path="/home" element={<Home/> } />
+       <Route path='/carts' element={<Carts/>} />
         <Route path="/panelAdmin" element={<AdminPanel />}/>
         <Route path='/*' element={<NotFound/>}/>
         <Route path="paymentgateway" element={<Paymentgateway/>}/>
+        <Route path="/basket" element={<Basket/>}/>
+        <Route path="/finalAuth" element={<FinalAuth/>}/>
+        <Route path="/productDetail" element={<CartDetail/>}/>
+        </Route>
       </Routes>
-    </BrowserRouter>
+</BrowserRouter>
   );
 };
 
